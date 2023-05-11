1382e6d582b1af8c37ac02eb83a73bf4d4165ad4
Dasha Khlistun <dashakhlistun@macbook-air.myfiosgateway.com>
2023-05-11 18:22:57 -0400

    first commit

---
+++ js/mobile-menu.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector(".btn");
+
+btn.addEventListener('click', openDocument);
+
+function openDocument() {
+    window.open('path/to/document.pdf', '_blank');
+}
+
+
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+    });
+})();
